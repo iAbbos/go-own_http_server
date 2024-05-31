e89162bd04e7d76ad00ffc37e4a16b1857096bdf
iAbbos <50768679+iAbbos@users.noreply.github.com>
2024-05-31 17:51:30 +0500

    Content-Encoding header 2

--- internal/usecase/echo.go
+++ internal/usecase/echo.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"github.com/codecrafters-io/http-server-starter-go/internal/entity"
 	"strings"
 )
@@ -16,7 +17,12 @@ func Echo(req entity.Request) *entity.Response {
 	resp.SetStatus(200, "OK")
 	resp.SetHeader("Content-Type", "text/plain")
 
+	fmt.Println("req.Headers: ", req.Headers)
 	encoding, ok := req.Headers["Accept-Encoding"]
+
+	fmt.Println("encoding: ", encoding)
+	fmt.Println("ok: ", ok)
+
 	if encoding == "gzip" && ok {
 		resp.SetHeader("Accept-Encoding", encoding)
 	}
